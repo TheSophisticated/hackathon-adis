9f0c590b7eb33d318c41913ab032d18cd32aef79
TheLegend <amoghvarote@gmail.com>
2023-10-30 23:49:03 +0400

    chatbot done YESGOOO

--- pages/Food-Guide.py
+++ pages/Food-Guide.py
@@ -1,9 +1,12 @@
 import json
 import requests  # pip install requests
 import streamlit as st  # pip install streamlit
+import openai #pip install openai
 from streamlit_lottie import st_lottie  # pip install strclseamlit-lottie
 from streamlit_option_menu import option_menu
+from streamlit_chat import message
 from streamlit_extras.switch_page_button import switch_page
+
 
 hide_st_style = """
             <style>
@@ -25,3 +28,43 @@ selected = option_menu(
 
 with st.container():
     st.title("Food Guide ")
+
+    
+openai.api_key = st.secrets["api_secret"]
+    
+def generate_response(prompt):
+    completions= openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature = 0.3,
+    )
+    message = completions.choices[0].text
+    return message
+
+#storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+    
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    #store the output
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
