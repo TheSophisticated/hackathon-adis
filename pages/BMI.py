28916417c9d9bf99d83dc1475d67cb3fa1322010
TheLegend <amoghvarote@gmail.com>
2023-11-05 16:21:12 +0400

    s

--- pages/BMI.py
+++ pages/BMI.py
@@ -15,7 +15,7 @@ st.title('         ‎ ‎ ‎ ‎ ‎ ‎ ‎ (BMI) Body Mass Index         ')
 st.image(image, caption='BMI Scale')
 
 st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)
      ''')
