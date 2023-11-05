c0eeb39a590ced541330468e60c4abb56a845b01
TheLegend <amoghvarote@gmail.com>
2023-11-05 16:21:57 +0400

    ss

--- pages/BMI.py
+++ pages/BMI.py
@@ -16,9 +16,9 @@ st.image(image, caption='BMI Scale')
 
 st.latex(r'''
 a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+\sum_{k=0}^{n-1} ar^k =
+a \left(\frac{1-r^{n}}{1-r}\right)
+''')
 
 st.header("Calculate Your BMI")
 
