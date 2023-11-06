a7ee45c93dc75a4327b5dfe9bebc21bb58c1af8b
TheLegend <amoghvarote@gmail.com>
2023-11-06 21:21:13 +0400

    Update Electricity.py

--- pages/Electricity.py
+++ pages/Electricity.py
@@ -8,6 +8,8 @@ from streamlit_lottie import st_lottie  # pip install strclseamlit-lottie
 from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
 
+st.set_page_config(page_title="Real Time Energy Usage",
+                   layout="wide")
 
 hide_st_style = """
             <style>
@@ -34,8 +36,7 @@ df = pd.read_excel(
 
 print (df)
 
-st.set_page_config(page_title="Real Time Energy Usage",
-                   layout="wide")
+
 
 
 
